b91791e5f87b627a9c437ad0bc36b830e77e3de6
Carlos Lassance <cadurosar@gmail.com>
2020-03-05 14:55:58 +0100

    add tests

--- signals_over_graphs_baselines.sh
+++ signals_over_graphs_baselines.sh
@@ -7,6 +7,23 @@ do
             for graph_type in Cosine RBF Covariance
             do
                 time python generate_graph.py --dataset $dataset --normalization $normalization --graph_type $graph_type --nn $nn
+                time python generate_graph.py --dataset $dataset --normalization $normalization --graph_type $graph_type --nn $nn --minmaxscaler
+            done
+        done
+        
+    done
+done
+
+for dataset in cora flowers102 ESC-50
+do
+    for nn in 5 10 20 30 40 50 100 200 500 1000
+    do
+        for normalization in None BothSides
+        do
+            for graph_type in Cosine RBF Covariance L2Distance
+            do
+                time python generate_graph.py --dataset $dataset --normalization $normalization --graph_type $graph_type --nn $nn --nnk
+                time python generate_graph.py --dataset $dataset --normalization $normalization --graph_type $graph_type --nn $nn --minmaxscaler --nnk
             done
         done
         
